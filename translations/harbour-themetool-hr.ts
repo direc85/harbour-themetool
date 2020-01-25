c1f8c6f9cbc68259c17828214a65c0b01b8dc5d8
milotype <43657314+milotype@users.noreply.github.com>
2020-01-25 20:56:27 +0100

    Added croatian translation

---
+++ translations/harbour-themetool-hr.ts
@@ -0,0 +1,229 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hr">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>A simple tool to review the current ambience colors and element sizes.</source>
+        <translation>Jednostavan alat za pregled trenutačnih boja i veličina elemenata.</translation>
+    </message>
+    <message>
+        <source>Finnish</source>
+        <translation>Finski</translation>
+    </message>
+    <message>
+        <source>Swedish</source>
+        <translation>Švedski</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <comment>License: GPLv2</comment>
+        <translation>Licenca</translation>
+    </message>
+    <message>
+        <source>Source code is available at GitHub. Translations, bug reports and other contributions are welcome!</source>
+        <translation>Izvorni kod je dostupan na GitHub-u. Prijevodi, izvještaji o greškama i ostali doprinosi su dobrodošli!</translation>
+    </message>
+    <message>
+        <source>Chinese</source>
+        <translation>Kineski</translation>
+    </message>
+    <message>
+        <source>Croatian</source>
+        <translation>Hrvatski</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Theme
+Tool</source>
+        <translation>Alat za
+dizajn ekrana</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Theme Tool</source>
+        <translation>Alat za dizajn ekrana</translation>
+    </message>
+    <message>
+        <source>Ambience info</source>
+        <translation>Podaci o dizajnu</translation>
+    </message>
+    <message>
+        <source>Display Name</source>
+        <translation>Ime</translation>
+    </message>
+    <message>
+        <source>Favorite</source>
+        <translation>Favorit</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Da</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>Ne</translation>
+    </message>
+    <message>
+        <source>Font Sizes</source>
+        <translation>Veličine fonta</translation>
+    </message>
+    <message>
+        <source>System font size</source>
+        <translation>Veličina fonta sustava</translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <translation>Normalno</translation>
+    </message>
+    <message>
+        <source>Large</source>
+        <translation>Veliko</translation>
+    </message>
+    <message>
+        <source>Huge</source>
+        <translation>Ogromno</translation>
+    </message>
+    <message>
+        <source>Sample texts</source>
+        <translation>Primjeri</translation>
+    </message>
+    <message>
+        <source>Tiny</source>
+        <translation>Sitno</translation>
+    </message>
+    <message>
+        <source>Extra Small</source>
+        <translation>Manje</translation>
+    </message>
+    <message>
+        <source>Small</source>
+        <translation>Malo</translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation>Srednje</translation>
+    </message>
+    <message>
+        <source>Extra Large</source>
+        <translation>Jako veliko</translation>
+    </message>
+    <message>
+        <source>Icon Sizes</source>
+        <translation>Veličina ikona</translation>
+    </message>
+    <message>
+        <source>Launcher</source>
+        <translation>Pokretač</translation>
+    </message>
+    <message>
+        <source>Colors</source>
+        <translation>Boje</translation>
+    </message>
+    <message>
+        <source>Default Colors</source>
+        <translation>Standardne boje</translation>
+    </message>
+    <message>
+        <source>Primary Color</source>
+        <translation>Glavna boja</translation>
+    </message>
+    <message>
+        <source>Secondary Color</source>
+        <translation>Sporedna boja</translation>
+    </message>
+    <message>
+        <source>Highlight Color</source>
+        <translation>Boja isticanja</translation>
+    </message>
+    <message>
+        <source>Highlight Dimmer Color</source>
+        <translation>Boja isticanja zatamnjenja</translation>
+    </message>
+    <message>
+        <source>Highlight Background Color</source>
+        <translation>Boja isticanja pozadine</translation>
+    </message>
+    <message>
+        <source>Secondary Highlight Color</source>
+        <translation>Sporedna boja isticanja</translation>
+    </message>
+    <message>
+        <source>Overlay Background Color</source>
+        <translation>Boja prekrivanja pozadine</translation>
+    </message>
+    <message>
+        <source>Dark Colors</source>
+        <translation>Tamne boje</translation>
+    </message>
+    <message>
+        <source>Dark Primary Color</source>
+        <translation>Glavna tamna boja</translation>
+    </message>
+    <message>
+        <source>Dark Secondary Color</source>
+        <translation>Sporedna tamna boja</translation>
+    </message>
+    <message>
+        <source>Light Colors</source>
+        <translation>Svjetle boje</translation>
+    </message>
+    <message>
+        <source>Light Primary Color</source>
+        <translation>Glavna svjetla boja</translation>
+    </message>
+    <message>
+        <source>Light Secondary Color</source>
+        <translation>Sporedna svjetla boja</translation>
+    </message>
+    <message>
+        <source>Presence Mode Colors</source>
+        <translation>Boje modusa prisutnosti</translation>
+    </message>
+    <message>
+        <source>Available Color</source>
+        <translation>Dostupno</translation>
+    </message>
+    <message>
+        <source>Away Color</source>
+        <translation>Neprisutno</translation>
+    </message>
+    <message>
+        <source>Busy Color</source>
+        <translation>Zauzeto</translation>
+    </message>
+    <message>
+        <source>Offline Color</source>
+        <translation>Neumreženo</translation>
+    </message>
+    <message>
+        <source>Other Colors</source>
+        <translation>Ostale boje</translation>
+    </message>
+    <message>
+        <source>Error Color</source>
+        <translation>Boje greškaka</translation>
+    </message>
+    <message>
+        <source>Button Sizes</source>
+        <translation>Veličine gumbova</translation>
+    </message>
+    <message>
+        <source>XSmall</source>
+        <comment>Must fit in the button.</comment>
+        <translation>Jako malo</translation>
+    </message>
+    <message>
+        <source>Cover Sizes</source>
+        <translation>Veličine naslovnih stranica</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Informacije</translation>
+    </message>
+</context>
+</TS>
