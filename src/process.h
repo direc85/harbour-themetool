f5c0469232c02ae9c65d187127c0458c3aa6ca58
Matti Viljanen <matti.viljanen@kapsi.fi>
2021-04-25 12:39:39 +0300

    Use QStringList instead of QVariantList

--- src/process.h
+++ src/process.h
@@ -5,27 +5,15 @@
 // Copyright © 2015 John Temples
 
 #include <QProcess>
-#include <QVariant>
-//#include <QDebug>
 
 class Process : public QProcess {
     Q_OBJECT
 
 public:
-    Process(QObject *parent = nullptr) : QProcess(parent) {
+    Process(QObject *parent = nullptr) : QProcess(parent) { }
 
-    }
-
-    Q_INVOKABLE void start(const QString &program, const QVariantList &arguments) {
-        QStringList args;
-
-        // convert QVariantList from QML to QStringList for QProcess
-
-        for (int i = 0; i < arguments.length(); i++)
-            args << arguments[i].toString();
-
-        //qDebug() << program + " " + args.join(" ");
-        QProcess::start(program, args);
+    Q_INVOKABLE void start(const QString &program, const QStringList &arguments) {
+        QProcess::start(program, arguments);
         QProcess::waitForStarted(1000);
         QProcess::waitForFinished(1000);
     }
